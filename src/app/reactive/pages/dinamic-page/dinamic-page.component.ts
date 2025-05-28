3e16c68ca94132ebcd5ffab3ff05671a94238c94
Omar Diaz <odiaz.dev@gmail.com>
2025-05-28 16:02:07 -0600

    rutas padre y template html

--- src/app/reactive/pages/dinamic-page/dinamic-page.component.ts
+++ src/app/reactive/pages/dinamic-page/dinamic-page.component.ts
@@ -1,8 +1,9 @@
+import { JsonPipe } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-dinamic-page',
-  imports: [],
+  imports: [JsonPipe],
   templateUrl: './dinamic-page.component.html',
 })
 export class DinamicPageComponent { }
