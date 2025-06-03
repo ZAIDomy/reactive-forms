46c00efe75e624e4dbf0357156130e74ee34db2c
Alejandro Diaz <odiaz.dev@gmail.com>
2025-06-02 19:05:00 -0600

    errores en pantalla

--- src/app/reactive/pages/basic-page/basic-page.component.ts
+++ src/app/reactive/pages/basic-page/basic-page.component.ts
@@ -25,16 +25,28 @@ export class BasicPageComponent {
     inStorage: new FormControl(0),
   });*/
 
-  isValidField(fieldName: string): boolean|null {
+  isValidField(fieldName: string): boolean | null {
     return !!this.myForm.controls[fieldName].errors;
   }
 
-  getFieldError(fieldName: string): string|null {
+  getFieldError(fieldName: string): string | null {
 
-    if(this.myForm.controls[fieldName]) return null;
+    if(!this.myForm.controls[fieldName]) return null;
 
     const errors = this.myForm.controls[fieldName].errors ?? {};
 
+    for(const key of Object.keys(errors)){
+      switch(key){
+        case 'required':
+          return 'Este campo es requerido';
+        case 'minlength':
+          return `Minimo de ${errors['minlength'].requiredLength } caracteres`;
+        case 'min':
+          return `Valor minimo de ${errors['min'].min }`;
+      }
+    }
+    return null;
+
   }
 
-} 
+}
