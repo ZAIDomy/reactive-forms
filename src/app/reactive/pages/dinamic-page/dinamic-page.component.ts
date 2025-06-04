e172d95c09b8589689a2ebf0c290c0211e4d33eb
Omar Diaz <odiaz.dev@gmail.com>
2025-06-04 14:34:15 -0600

    agregar y eliminar controles de forms

--- src/app/reactive/pages/dinamic-page/dinamic-page.component.ts
+++ src/app/reactive/pages/dinamic-page/dinamic-page.component.ts
@@ -1,6 +1,6 @@
 import { JsonPipe } from '@angular/common';
 import { Component, inject } from '@angular/core';
-import { FormArray, FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
+import { FormArray, FormBuilder, FormControl, ReactiveFormsModule, Validators } from '@angular/forms';
 import { FormUtils } from '../../../utils/form-utils';
 
 @Component({
@@ -9,6 +9,7 @@ import { FormUtils } from '../../../utils/form-utils';
   templateUrl: './dinamic-page.component.html',
 })
 export class DinamicPageComponent {
+
   private fb = inject(FormBuilder);
   formUtils = FormUtils;
 
@@ -22,12 +23,28 @@ export class DinamicPageComponent {
   )
   });
 
+  newFavorite = new FormControl('',Validators.required)
+  /* newFavoriteGame = this.fb.control([]); */
+
   get favoriteGames(){
     return this.myForm.get('favoriteGames') as FormArray;
   }
 
+  onAddToFavorites(){
+    if(this.newFavorite.invalid) return;
+    const newGame = this.newFavorite.value;
 
+    this.favoriteGames.push(this.fb.control(newGame,Validators.required));
 
+    this.newFavorite.reset();
+  }
 
+  onDeleteFavorite(index: number) {
+    this.favoriteGames.removeAt(index);
+  }
+
+  onSubmit(){
+    this.myForm.markAllAsTouched();
+  }
 
 }
